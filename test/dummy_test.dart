13d9b3658a1824ec7e408861b133db2c648a2979
hotchiler <141277408+hotchiler@users.noreply.github.com>
2025-05-19 17:57:22 -0700

    dummy test had syntax err

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-p|4120
-\=-
-]0[void main() {
+void main() {
   test('dummy test to verify test infra', () {
     expect(2 + 2, equals(4));
   });
