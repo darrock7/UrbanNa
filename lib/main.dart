c4ec29b8d33105ada0ab0b46d350c47a1c32bc00
Jean Choe <jeannchoee@gmail.com>
2025-05-20 13:40:36 -0700

    Report submission correction
    IOS submission request fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,21 @@ import 'package:provider/provider.dart';
 import 'package:urbanna/screens/home_screen.dart';  
 import 'package:urbanna/providers/report_provider.dart';  
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:urbanna/helpers/data_helper.dart';
 
-void main() {
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
+
+  await deleteOldDb();
 
   runApp(
     ChangeNotifierProvider(
@@ -28,8 +37,22 @@ class MyApp extends StatelessWidget {
       title: 'UrbanNa Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'UrbanNa'),  
     );
+  }
+}
+
+Future<void> deleteOldDb() async {
+  final dir = await getDatabasesPath();
+  final path = join(dir, DataHelper.dataStorage);
+  final dbFile = File(path);
+
+  if (await dbFile.exists()) {
+    await dbFile.delete();
+    print('🗑️ Deleted old database at: $path');
+  } else {
+    print('ℹ️ No existing database found at: $path');
   }
 }
